6e9a9eace662dd09de13c6e493153c32368bc395
gnanaprakash2004 <96563884+gnanaprakash2004@users.noreply.github.com>
2021-12-30 10:15:56 +0530

    Add files via upload

---
+++ add.c
@@ -0,0 +1,12 @@
+/* a c program to perform addition */
+# include <stdio.h>
+int main ()
+      {
+      	int a,b,c;
+      	prinf("enter any two vaues");
+      	scanf("%d%d",&a,&b);
+      	c=a+b;
+      	printf("the addition of %d and %d is %d",a,b,c);
+      	return 0;
+      	
+	  }
