c98aa7ef1e5110530248271e4681ac8b7ecce230
gnanaprakash2004 <96563884+gnanaprakash2004@users.noreply.github.com>
2021-12-30 10:11:02 +0530

    Add files via upload

---
+++ c program on divisibiliy.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+void main()
+{
+	int number;
+	printf("enter the number");
+	scanf("%d",&number);
+	if(number%1==0)
+	{
+		printf("divisible by 1");
+		
+	if(number%2==0)
+		printf("\ndivisible by 2");
+	
+	if(number%3==0)
+		printf("\ndivisible by 3");
+	if(number%4==0)
+		printf("\ndivisible by 4");
+	if(number%5==0)
+		printf("\ndivisible by 5");
+	if(number%6==0)
+		printf("\ndivisible by 6");
+	if(number%7==0)
+		printf("\ndivisible by 7");
+	if(number%8==0)
+		printf("\ndivisible by 8");
+	if(number%9==0)
+		printf("\ndivisible by 9");	
+	}
+	
+}
