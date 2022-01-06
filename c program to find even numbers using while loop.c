8574a2f6e012e2b8a88d1563d3937fdcb5ae71ea
Gnana Prakash Pragada <96563884+gnanaprakash2004@users.noreply.github.com>
2022-01-06 20:42:26 +0530

    Add files via upload

---
+++ c program to find even numbers using while loop.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	int i,n,j;
+	scanf("%d%d",&i,&n);
+	while(i<=n)
+	{
+		if(i%2==0)
+		{
+			printf("\n %d",i);
+		
+		}
+			i++;
+	}
+}
---
+++ c program to find odd numbers using while loop.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	int i,n,j;
+	scanf("%d%d",&i,&n);
+	while(i<=n)
+	{
+		if(i%2==1)
+		{
+			printf("\n %d",i);
+		
+		}
+			i++;
+	}
+}
